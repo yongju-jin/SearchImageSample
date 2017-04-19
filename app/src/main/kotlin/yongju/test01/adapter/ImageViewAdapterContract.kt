e18fde3d004015dd45c1421048b25deb47018d87
yongju <yong8521@gmail.com>
2017-04-19 15:56:37 +0900

    - interface 주석 추가

--- app/src/main/kotlin/yongju/test01/adapter/ImageViewAdapterContract.kt
+++ app/src/main/kotlin/yongju/test01/adapter/ImageViewAdapterContract.kt
@@ -1,19 +1,16 @@
-package yongju.test01.adapter
+package yongju.lezhin.adapter
 
-import yongju.test01.data.ImageInfo
+import yongju.lezhin.data.ImageInfo
 
 
 /**
  * Created by yongju on 2017. 4. 6..
  */
 interface ImageViewAdapterContract {
-    // adapter 갱신
     interface View {
         fun reload()
-        fun clearImage()
     }
 
-    // adapter 에 data 전달
     interface Model {
         fun addItem(imageInfo: ImageInfo)
     }
